75763a3cb6655e11c3bddc63c7c7cb23d5637178
Mariia Kovalova <mariia_kovalova@ukr.net>
2023-08-24 23:35:25 +0300

    smooth scroll hero

--- components/Hero/components/BtnLink.tsx
+++ components/Hero/components/BtnLink.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { FC } from 'react';
 import BtnFrame from './BtnFrame';
 import Link from 'next/link';
@@ -7,24 +9,37 @@ interface IProps {
   href: string;
 }
 
-const BtnLink: FC<IProps> = ({ text, href }) => (
-  <Link
-    href={href}
-    className="relative block px-[18px] py-[3px] w-[280px]
+const BtnLink: FC<IProps> = ({ text, href }) => {
+  const handleScroll = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+    e.preventDefault();
+    const targetId = e.currentTarget.href.replace(/.*\#/, '');
+    const elem = document.getElementById(targetId);
+    window.scrollTo({
+      top: elem?.getBoundingClientRect().top,
+      behavior: 'smooth',
+    });
+  };
+
+  return (
+    <Link
+      href={href}
+      onClick={handleScroll}
+      className="relative block px-[18px] py-[3px] w-[280px]
                bg-transparent-white-light transition-transform
                hover:bg-transparent-white-light-accent 
                focus:bg-transparent-white-light-accent
                 tablet:py-[1px] tablet:w-full 
                 desktop:p-[11px]"
-  >
-    <p
-      className="text-[18px] font-bold leading-[48px] uppercase text-center 
-                  desktop:text-[32px]"
     >
-      {text}
-    </p>
-    <BtnFrame />
-  </Link>
-);
+      <p
+        className="text-[18px] font-bold leading-[48px] uppercase text-center 
+                  desktop:text-[32px]"
+      >
+        {text}
+      </p>
+      <BtnFrame />
+    </Link>
+  );
+};
 
 export default BtnLink;
