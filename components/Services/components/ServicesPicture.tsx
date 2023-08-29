b7fcfd3e2f3909ffca2ac6778809f368ba5248dc
Mariia Kovalova <mariia_kovalova@ukr.net>
2023-08-29 09:40:58 +0300

    images:removed sizes

--- components/Services/components/ServicesPicture.tsx
+++ components/Services/components/ServicesPicture.tsx
@@ -13,19 +13,16 @@ const ServicesPicture: FC<IProps> = ({ alt, number }) => {
       <source
         srcSet={`/images/services-photo-${number}@x1.jpg 1x, /images/services-photo-${number}@x2.jpg 2x`}
         media={`(min-width: ${desktop})`}
-        sizes="607px"
         type="image/jpeg"
       />
       <source
         srcSet={`/images/services-photo-${number}-tablet@x1.jpg 1x, /images/services-photo-${number}-tablet@x2.jpg 2x`}
         media={`(min-width: ${tablet})`}
-        sizes="463px"
         type="image/jpeg"
       />
       <source
         srcSet={`/images/services-photo-${number}-mobile@x1.jpg 1x, /images/services-photo-${number}-mobile@x2.jpg 2x`}
         media={`(min-width: ${min})`}
-        sizes="280px"
         type="image/jpeg"
       />
       <Image
