6114d3d60e8aceb5baa707720bccddaadf18207b
Mariia Kovalova <mariia_kovalova@ukr.net>
2023-08-31 19:21:36 +0300

    bug: local storage fix

--- utils/localStorageAPI.ts
+++ utils/localStorageAPI.ts
@@ -1,26 +1,32 @@
 const save = (key: string, value: any) => {
-  try {
-    const serializedState = JSON.stringify(value);
-    localStorage.setItem(key, serializedState);
-  } catch (error) {
-    console.error('Set state error: ', error);
+  if (typeof window !== 'undefined' && window.localStorage) {
+    try {
+      const serializedState = JSON.stringify(value);
+      localStorage.setItem(key, serializedState);
+    } catch (error) {
+      console.error('Set state error: ', error);
+    }
   }
 };
 
 const load = (key: string) => {
-  try {
-    const serializedState = localStorage.getItem(key);
-    return serializedState === null ? undefined : JSON.parse(serializedState);
-  } catch (error) {
-    console.error('Get state error: ', error);
+  if (typeof window !== 'undefined' && window.localStorage) {
+    try {
+      const serializedState = localStorage.getItem(key);
+      return serializedState === null ? undefined : JSON.parse(serializedState);
+    } catch (error) {
+      console.error('Get state error: ', error);
+    }
   }
 };
 
 const remove = (key: string) => {
-  try {
-    localStorage.removeItem(key);
-  } catch (error) {
-    console.error('Remove state error: ', error);
+  if (typeof window !== 'undefined' && window.localStorage) {
+    try {
+      localStorage.removeItem(key);
+    } catch (error) {
+      console.error('Remove state error: ', error);
+    }
   }
 };
 
